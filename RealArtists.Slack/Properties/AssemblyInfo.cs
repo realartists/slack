0b2f17708e96bcc00b7b31a3249ec4d9b4a35065
Nick Sivo <nick@kogir.com>
2015-12-02 17:58:28 -0800

    Nuget package.

--- RealArtists.Slack/Properties/AssemblyInfo.cs
+++ RealArtists.Slack/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RealArtists.Slack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple C# Slack Web API client library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Real Artists, Inc.")]
 [assembly: AssemblyProduct("RealArtists.Slack")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright 2015 Real Artists, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
